179adec916a9fbfef6dfc8758035e456118b0fff
Mark Keinhörster <mark.keinhoerster@googlemail.com>
2022-11-18 21:24:19 +0100

    update for rotary

--- keyboards/planck/keymaps/en-umlaute/keymap.c
+++ keyboards/planck/keymaps/en-umlaute/keymap.c
@@ -20,7 +20,6 @@
 
 enum planck_layers {
   _QWERTY,
-  _NUMPAD,
   _SYMBOLS,
   _UMLAUTS,
   _ADJUST
@@ -28,7 +27,6 @@ enum planck_layers {
 
 #define LOWER MO(_UMLAUTS)
 #define RAISE MO(_SYMBOLS)
-#define FN TT(_NUMPAD)
 #define ADJUST MO(_ADJUST)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    FN, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+    KC_DEL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
 ),
 
 
@@ -46,14 +44,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_GRV,
     _______,    KC_MINS,     KC_EQL,    KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_UNDS,    KC_PLUS,    KC_LCBR,    KC_RCBR,     KC_PIPE,    KC_TILDE,
     XXXXXXX,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
-),
-
-
-[_NUMPAD] = LAYOUT_planck_grid(
-    XXXXXXX,  XXXXXXX,     KC_UP,       XXXXXXX,    XXXXXXX,   KC_DEL,           KC_KP_7,       KC_KP_8,     KC_KP_9,        KC_EQL,        XXXXXXX,    _______,
-    XXXXXXX,  KC_LEFT,     KC_DOWN,     KC_RIGHT,   XXXXXXX,   KC_KP_ASTERISK,   KC_KP_4,       KC_KP_5,     KC_KP_6,        KC_KP_SLASH,   XXXXXXX,    XXXXXXX,
-    _______,  XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX,   KC_KP_PLUS,       KC_KP_1,       KC_KP_2,     KC_KP_3,        KC_KP_MINUS,   XXXXXXX,    KC_KP_ENTER,
-    _______,  _______,     _______,     _______,    _______,   _______,          _______,       _______,     KC_KP_0,        KC_KP_DOT,     XXXXXXX,    XXXXXXX
 ),
 
 
@@ -82,17 +72,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             break;
         case 1:
             rgblight_enable_noeeprom();
-            rgblight_sethsv_noeeprom(HSV_BLUE);
+            rgblight_sethsv_noeeprom(HSV_GREEN);
             break;
         case 2:
             rgblight_enable_noeeprom();
-            rgblight_sethsv_noeeprom(HSV_GREEN);
-            break;
-        case 3:
-            rgblight_enable_noeeprom();
             rgblight_sethsv_noeeprom(HSV_RED);
             break;
-        case 4:
+        case 3:
             rgblight_enable_noeeprom();
             rgblight_sethsv_noeeprom(HSV_GOLD);
             break;
@@ -103,4 +89,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 void keyboard_post_init_user(void) {
     rgblight_disable_noeeprom();
     rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_RIGHT);
+    } else {
+        tap_code(KC_LEFT);
+    }
+    return false;
 }
