bd810258dacccc437fd8aab64819adfe313e5b03
mark <mark.keinhoerster@gmail.com>
2022-12-22 22:00:47 +0100

    update keymaps for planck and lumberjack

--- keyboards/planck/keymaps/en-umlaute/keymap.c
+++ keyboards/planck/keymaps/en-umlaute/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_DEL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
+    KC_MUTE, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT
 ),
 
 
@@ -43,15 +43,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    _______,
     _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_GRV,
     _______,    KC_MINS,     KC_EQL,    KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_UNDS,    KC_PLUS,    KC_LCBR,    KC_RCBR,     KC_PIPE,    KC_TILDE,
-    XXXXXXX,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______
 ),
 
 
 [_UMLAUTS] = LAYOUT_planck_grid(
-    XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  DE_UDIA, XXXXXXX, DE_ODIA, XXXXXXX, XXXXXXX,
-    XXXXXXX,  DE_ADIA,   DE_SS,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    _______,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX,  _______,   _______,   _______,   _______,   _______,   _______,  _______, _______, _______, _______, _______
+    XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  DE_UDIA, XXXXXXX,        DE_ODIA,        XXXXXXX,        XXXXXXX,
+    XXXXXXX,  DE_ADIA,   DE_SS,     XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
+    _______,  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX,        XXXXXXX,        XXXXXXX,        XXXXXXX,
+    _______,  _______,   _______,   _______,   _______,   _______,   _______,  _______, KC_MS_WH_RIGHT, KC_MS_WH_UP,    KC_MS_WH_DOWN,  KC_MS_WH_LEFT
 ),
 
 
@@ -93,9 +93,9 @@ void keyboard_post_init_user(void) {
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (clockwise) {
-        tap_code(KC_RIGHT);
+        tap_code(KC_VOLU);
     } else {
-        tap_code(KC_LEFT);
+        tap_code(KC_VOLD);
     }
     return false;
 }
